c4165797ce41013878d65ae5a380d0bb3f13d175
SuperHotDog <115141367+SuperHotDogCat@users.noreply.github.com>
2024-08-07 10:10:46 +0900

    Add files via upload

---
+++ test/benchmark/fib5.c
@@ -0,0 +1,18 @@
+long fib(long n) {
+  if (n < 2) {
+    return 1;
+  } else {
+    long x;
+    long y;
+    x = fib(n - 1);
+    y = fib(n - 2);
+    return x + y;
+  }
+}
+long main(long n) {
+  long m;
+  long y;
+  m = 5;
+  y = fib(m);
+  return y;
+}
